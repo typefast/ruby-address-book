4933c50662ae3e0b3b1050ca6f839f80a251040f
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-07 15:50:48 +0000

    Add find_by_name method

--- address_book.rb
+++ address_book.rb
@@ -6,6 +6,23 @@ class AddressBook
     @contacts = []
   end
   
+  def find_by_name(name)
+    results = []
+    search = name.downcase
+    contacts.each do |contact|
+      if contact.full_name.downcase.include?(search)
+        results.push(contact)
+      end
+    end
+    puts "Name search results (#{search})"
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
+  end
+  
   def print_contact_list
     puts "Contact List"
     contacts.each do |contact|
@@ -23,4 +40,5 @@ harry.add_phone_number("Work", "07866384844")
 harry.add_phone_number("Play", "07866456732")
 harry.add_address("home", "main st", "", "Oxford", "Oxon", "123456")
 address_book.contacts.push(harry)
-address_book.print_contact_list
+#address_book.print_contact_list
+address_book.find_by_name('l')
